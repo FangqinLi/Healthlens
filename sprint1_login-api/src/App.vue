835b23facc5ae6d15b85e58bff7a844c8ab28c1c
71119223zhengyuxiang <1259251221@qq.com>
2024-10-17 17:28:17 -0400

    commit by Yuhsiang Cheng

---
+++ sprint1_login-api/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+html,
+body {
+  height: 100%;
+}
+
+body {
+  display: flex;
+  align-items: center;
+  background-color: #333;
+}
+</style>
+
+
