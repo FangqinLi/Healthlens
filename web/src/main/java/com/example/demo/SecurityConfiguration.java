b9e6478cf79b9654f391cc6e04d9ca5d8d24e335
xiaorui-069 <1239058750@qq.com>
2024-11-14 22:34:24 -0500

    merge

--- web/src/main/java/com/example/demo/SecurityConfiguration.java
+++ web/src/main/java/com/example/demo/SecurityConfiguration.java
@@ -51,7 +51,6 @@ public class SecurityConfiguration {
                         .permitAll()
                 )
                 .oauth2Login(Customizer.withDefaults());
-//                .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults());
 
         return http.build();
     }
